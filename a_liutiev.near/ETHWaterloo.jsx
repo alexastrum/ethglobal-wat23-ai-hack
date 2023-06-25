03f6489641a1585f7ed960a2a484152010d5e960
Alex Astrum <alexastrum@gmail.com>
2023-06-25 08:47:24 -0400

    Update ETHWaterloo.jsx

--- a_liutiev.near/ETHWaterloo.jsx
+++ a_liutiev.near/ETHWaterloo.jsx
@@ -2,18 +2,29 @@ State.init({
   value: "submit",
   inputSubmitLabel: "Type Message",
   web3connectLabel: "Connect Wallet",
-  emptyMessage: "start the thread pussy...",
+  emptyMessage: "start the thread boss...",
+  walletMessage: "View wallet data on AirStack",
+  walletAddress: "",
 
   messageCount: 0,
   inputValue: "",
   submitMessage: "",
   messageArray: [],
-});
+  address: undefined,
+  sender: undefined,
+  balance,
+  undefined,
 
-//API https://us-central1-ethglobal-wat23-ai-hack.cloudfunctions.net/helloWorld?q=tell+me+a+joke+about+BOS
+  tokenData: "",
+});
 
 const API_URL =
   "https://us-central1-ethglobal-wat23-ai-hack.cloudfunctions.net/helloWorld";
+
+const AIR_API_KEY = "6e4d51488a7546c5b9ee7a048ec3fc57";
+const AIR_API = "https://api.airstack.xyz/gql";
+const QN_API =
+  "https://frequent-purple-silence.discover.quiknode.pro/4a03560450188f062f4f1cf6bd075ef784d54e65/";
 
 const value = state.value || "n/a";
 const web3connectLabel = state.web3connectLabel || "n/a";
@@ -21,35 +32,191 @@ const inputSubmitLabel = state.inputSubmitLabel || "n/a";
 const messageCount = state.messageCount || 0;
 const messageArray = state.messageArray || [];
 const emptyMessage = state.emptyMessage || "";
+const walletMessage = state.walletMessage || "";
+const sender = state.sender || "Alex Astrum";
+const walletAddress = state.walletAddress;
 
-// message counter submit
-const handleButtonClick = () => {
-  if (state.submitMessage !== "") {
+if (state.sender === undefined) {
+  const accounts = Ethers.send("eth_requestAccounts", []);
+  if (accounts.length) {
+    State.update({ sender: accounts[0], walletAddress: accounts[0] });
+    console.log("set sender", accounts[0]);
+  }
+}
+
+const getSender = () => {
+  return !state.sender
+    ? ""
+    : state.sender.substring(0, 2) +
+        ".." +
+        state.sender.substring(state.sender.length - 4, state.sender.length);
+};
+
+const handleButtonClick = async () => {
+  if (state.submitMessage !== "" && state.sender !== undefined) {
     State.update({
       messageCount: state.messageCount + 1,
       messageArray: [
         ...state.messageArray,
         {
           id: state.messageCount,
-          sender: "Alex 0x00",
+          sender: getSender(),
           date: new Date().toLocaleTimeString(),
           payload: state.submitMessage,
         },
       ],
     });
-    fetchData();
-  } else return;
+
+    fetchMessage().then((res) => {
+      const data = res.body;
+      State.update({
+        messageCount: state.messageCount + 1,
+        messageArray: [
+          ...state.messageArray,
+          {
+            id: state.messageCount,
+            sender: "AI",
+            date: new Date().toLocaleTimeString(),
+            payload: data.msg,
+          },
+        ],
+      });
+    });
+  } else if (state.submitMessage !== "") {
+    State.update({
+      emptyMessage: "Ever thought of signing in...?",
+    });
+  } else if (state.sender !== undefined) {
+    State.update({
+      emptyMessage: "Maybe you should try submitting something...",
+    });
+  }
 };
 
-const fetchData = () => {
-  console.log(state.messageArray[messageCount]);
+const fetchAccountBalances = () => {
+  fetchBalanceRequest().then((res) => {
+    let data = res.body;
+    data = data.data.TokenBalances.TokenBalance;
+    let tokenData = [];
+    fetchUSDCConverter(data);
+    // data.forEach(({ token, formattedAmount, tokenType }) => {
+    //   total += formattedAmount; // add formattedAmount to the running total
+    //   fetch
 
+    //   // Add a string to the tokenData array for each object in the data array
+    //   tokenData.push(
+    //     `- Token: ${
+    //       token.symbol
+    //     } - Token Type: ${tokenType} - Amount: ${formattedAmount.toFixed(2)}`
+    //   );
+    // });
+
+    State.update({
+      messageCount: state.messageCount + 1,
+      messageArray: [
+        ...state.messageArray,
+        {
+          id: state.messageCount,
+          sender: "AI",
+          date: new Date().toLocaleTimeString(),
+          payload:
+            `Here's more advanced token data from your wallet:
+            ` + tokenData,
+        },
+      ],
+    });
+  });
+};
+
+const fetchTokenData = () => {
+  fetchBalanceRequest().then((res) => {
+    let data = res.body;
+    console.log(data);
+    data = data.data.TokenBalances.TokenBalance;
+
+    let tokenData = data.map(
+      ({ tokenType, formattedAmount, tokenAddress }) =>
+        `
+        - Token Type: ${tokenType}, Amount: ${formattedAmount.toFixed(
+          2
+        )}, Token Address: ${
+          tokenAddress.substring(0, 4) + ".." + tokenAddress.slice(-4)
+        }`
+    );
+
+    State.update({
+      messageCount: state.messageCount + 1,
+      messageArray: [
+        ...state.messageArray,
+        {
+          id: state.messageCount,
+          sender: "AI",
+          date: new Date().toLocaleTimeString(),
+          payload: `Here's the token data from your wallet:
+            ${tokenData}`,
+        },
+      ],
+    });
+  });
+};
+
+const fetchWalletData = () => {
+  fetchWalletDataRequest().then((res) => {
+    let data = res.body;
+    data = data.data.Wallet.addresses;
+    let walletData = data[0];
+    State.update({
+      messageCount: state.messageCount + 1,
+      messageArray: [
+        ...state.messageArray,
+        {
+          id: state.messageCount,
+          sender: "AI",
+          date: new Date().toLocaleTimeString(),
+          payload: `Here are your wallet details: 
+          
+          - ${walletData}`,
+        },
+      ],
+    });
+  });
+};
+
+const fetchBalanceRequest = async () => {
+  let data =
+    '{"query":"query BalanceCheck {\\n  TokenBalances(\\n    input: {filter: {owner: {_in: [\\"' +
+    walletAddress +
+    '\\"]}}, blockchain: ethereum, limit: 10}\\n  ) {\\n    TokenBalance {\\n      tokenAddress\\n      amount\\n      formattedAmount\\n      tokenType\\n      token {\\n        name\\n        symbol\\n      }\\n    }\\n  }\\n}","operationName":"BalanceCheck"}';
+
+  return asyncFetch(AIR_API, {
+    body: data,
+    headers: {
+      "Content-Type": "application/json",
+      authorization: AIR_API_KEY,
+    },
+    method: "POST",
+  });
+};
+
+const fetchWalletDataRequest = async () => {
+  let data =
+    '{"query":"query wallets {\\n  Wallet(input: {identity: \\"' +
+    walletAddress +
+    '\\", blockchain: ethereum}) {\\n    addresses\\n  }\\n}","operationName":"wallets"}';
+  return asyncFetch(AIR_API, {
+    body: data,
+    headers: {
+      "Content-Type": "application/json",
+      authorization: AIR_API_KEY,
+    },
+    method: "POST",
+  });
+};
+
+const fetchMessage = async () => {
   let data = state.messageArray[messageCount];
   data = JSON.stringify(data);
-
-  console.log("SENT JSON object:", data);
-
-  asyncFetch(API_URL, {
+  return asyncFetch(API_URL, {
     body: data,
     headers: {
       "Content-Type": "application/json",
@@ -64,8 +231,6 @@ const handleInputChange = (e) => {
     submitMessage: e.target.value,
   });
 };
-
-function messageAI() {}
 
 return (
   <div class="card">
@@ -84,9 +249,25 @@ return (
 
     <Widget
       src="a_liutiev.near/widget/display_messages"
-      props={{ emptyMessage, messageArray }}
+      props={{ emptyMessage, messageArray, sender }}
     />
-    <br />
+    {messageCount > 0 ? (
+      <div class="d-flex justify-content-between align-items-center p-3 mb-0">
+        <div className="d-flex flex-row align-items-center">
+          <button class="btn btn-light me-2" onClick={fetchTokenData}>
+            Token Stats
+          </button>
+          <button class="btn btn-light me-2" onClick={fetchWalletData}>
+            Wallet Details
+          </button>
+          <button class="btn btn-light" onClick={fetchAccountBalances}>
+            Account Balance Check
+          </button>
+        </div>
+      </div>
+    ) : (
+      <></>
+    )}
     <div class="card-footer text-muted justify-content-start align-items-center p-3">
       <Widget
         src="a_liutiev.near/widget/input_submit"
@@ -98,6 +279,5 @@ return (
         }}
       />
     </div>
-    <div></div>
   </div>
 );
